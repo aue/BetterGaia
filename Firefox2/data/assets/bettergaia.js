5699854f23ad0a9c45717d3ad018d8c3e0ea069a
atthehotcorner <at.thehotcorner@gmail.com>
2014-10-16 01:38:55 -0400

    Firefox settings

--- Firefox2/data/assets/bettergaia.js
+++ Firefox2/data/assets/bettergaia.js
@@ -10,68 +10,81 @@ Copyright (c) BetterGaia and Bowafishtech
 //BetterGaia.insert.css('');
 
 function settingsOpen() {
-    BetterGaia.localFile('assets/settingsEmbed.html');
-    self.port.on('assets/settingsEmbed.html', function(data) {
-        $('html').append(data);
+    if ($('#BGSettings').length == 0) {
+        BetterGaia.localFile('assets/settingsEmbed.html');
+        self.port.on('assets/settingsEmbed.html', function(data) {
+            $('html').append(data);
+            $('#BGSLogo').prepend('<img src="' + BetterGaia.path('images/128.png') + '">');
 
-        // Set up menu
-        $('#BGSMenu').on('click', 'a:not(.active)', function() {
-            var name = $(this).attr('data-link');
-            $('#BGSMenu a.active, #BGSPages .page.active').removeClass('active');
-            $('#BGSPages .page.' + name).addClass('active');
-            $(this).addClass('active');
-        });
+            // Set up menu
+            $('#BGSMenu').on('click', 'a:not(.active)', function() {
+                var name = $(this).attr('data-link');
+                $('#BGSMenu a.active, #BGSPages .page.active').removeClass('active');
+                $('#BGSPages .page.' + name).addClass('active');
+                $(this).addClass('active');
+            });
 
-        // List installed extensions
-        if (Storage.data['extensions'] instanceof Array) {
-            for (var i = 0; i < Storage.data['extensions'].length; i++) {
-                var extension = Storage.data['extensions'][i];
+            $('#BGSCloseMenu').on('click', function() {
+                $('html').removeClass('BGSOpen');
+            });
 
-                $('#BGSPages .page.mybg .extensionsList').append('<a extension-id="' + extension.id + '">' + extension.title + '</a>');
+            // List installed extensions
+            if (Storage.data['extensions'] instanceof Array) {
+                for (var i = 0; i < Storage.data['extensions'].length; i++) {
+                    var extension = Storage.data['extensions'][i];
+
+                    $('#BGSPages .page.mybg .extensionsList').append('<a extension-id="' + extension.id + '">' + extension.title + '</a>');
+                }
             }
-        }
 
-        // Get list of extensions
-        $('#BGSMenu a[data-link="get"]').on('click.get', function() {
-            $(this).off('click.get');
-            $('#BGSPages .page.get').addClass('loaded');
+            // Get list of extensions
+            $('#BGSMenu a[data-link="get"]').on('click.get', function() {
+                $(this).off('click.get');
+                $('#BGSPages .page.get').addClass('loaded');
 
-            $.ajax({
-                url: BetterGaia.serverUrl + 'framework/extensions/list.json',
-                dataType: 'json'
-            }).done(function(data) {
-                if (data['enabled']) {
-                    $('#BGSPages .page.get p.offline').show();
-                    //return;
-                }
-
-                for (var i = 0; i < data['extensions'].length; i++) {
-                    if (Storage.data['extensionsInstalled'].indexOf(data['extensions'][i]['id']) == -1) {
-                        data['extensions'][i]['notInstalled'] = true;
+                $.ajax({
+                    url: BetterGaia.serverUrl + 'framework/extensions/list.json',
+                    dataType: 'json'
+                }).done(function(data) {
+                    $('#BGSPages .page.get .loading .default').addClass('hidden');
+                    if (!data['enabled']) {
+                        $('#BGSPages .page.get .loading .disabled').removeClass('hidden');
+                        return;
                     }
-                }
 
-                var template = Handlebars.compile($('#BGSPages .page.get ul').html());
-                var rendered = template(data);
-                $('#BGSPages .page.get ul').html(rendered);
+                    for (var i = 0; i < data['extensions'].length; i++) {
+                        if (Storage.data['extensionsInstalled'].indexOf(data['extensions'][i]['id']) == -1) {
+                            data['extensions'][i]['notInstalled'] = true;
+                        }
+                    }
 
-                //if (Storage.data['extensions'].indexOf(key)) {}
+                    var template = Handlebars.compile($('#BGSPages .page.get ul').html());
+                    var rendered = template(data);
+                    $('#BGSPages .page.get ul').html(rendered).removeClass('hidden');
+                    $('#BGSPages .page.get .loading').addClass('hidden');
 
-            }).fail(function(data) {
-                $('#BGSPages .page.get').text('Could not get extensions list.');
+                    //if (Storage.data['extensions'].indexOf(key)) {}
+
+                }).fail(function(data) {
+                    $('#BGSPages .page.get .loading .default').addClass('hidden');
+                    $('#BGSPages .page.get .loading .offline').removeClass('hidden');
+                });
+            });
+
+            // install new extensions
+            $('#BGSPages .page.get ul').on('click', 'li a.install', function() {
+                $(this).text('Installing...');
+                $(this).off('click.install');
+                BetterGaia.install.extension($(this).attr('data-id'), function(data) {
+                    if (data['success']) BetterGaia.console.log('Installed!');
+                    $('#BGSPages .page.get a[data-id="' + data['id'] + '"]').text('Added');
+                });
             });
         });
-
-        // install new extensions
-        $('#BGSPages .page.get ul').on('click', 'li a.install', function() {
-            $(this).text('Installing...');
-            $(this).off('click.install');
-            BetterGaia.install.extension($(this).attr('data-id'), function(data) {
-                if (data['success']) BetterGaia.console.log('Installed!');
-                $('#BGSPages .page.get a[data-id="' + data['id'] + '"]').hide();
-            });
-        });
-    });
+    }
+    
+    // open settings
+    $('html').addClass('BGSOpen');
 }
 
 if (window.location.hash === '#bgsettings') settingsOpen();
